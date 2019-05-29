27fc366a33000d1b04458423aa99142161d22726
ViniciusSabariz <vsabariz@hotmail.com>
2019-05-28 22:31:28 -0300

    Feito a decodificação

--- bin/main.dart
+++ bin/main.dart
@@ -3,16 +3,17 @@ import 'dart:convert';
 import 'dart:core';
 import 'package:http/http.dart' as http;
 
+//Link do arquivo Json
+const request = 'https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=02bb4223ace02495fd82a5e73053f7772b14a5dd';
+
+//Faz a requisição da api e retorna os dados
 Future<Map> getData() async {
     http.Response response = await http.get(request);
     //print(json.decode(response.body));
     return json.decode(response.body);
 }
 
-const request = 'https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=02bb4223ace02495fd82a5e73053f7772b14a5dd';
-
 void main() async{
-
     //Cria uma variável com o arquivo Json
     var codigo = await getData();
 
@@ -20,17 +21,33 @@ void main() async{
     int numero_casas = codigo["numero_casas"];
     String cifrado = codigo["cifrado"];
 
-    print(numero_casas);
-    print(codigo);
-    decoder(cifrado);
+    print (decoder(cifrado, numero_casas));
 }
 
-void decoder(String code){
-    List<String> Dicio = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "w", "y", "z"]; 
-
+// Decodificador
+String decoder(String code, int casas){
+    List<String> Dicio = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+    String result ='';
+    //Percorre a caractere por caractere na string
     for(int i=0; i<code.length; i++){
         var atual = code[i];
-        print(atual);
+        
+        //Verifica se o atual eh uma letra
+        if(Dicio.contains(atual)){
+            int index = Dicio.indexOf(atual);
+            int newIndex = index - casas;
+            if(newIndex<0){ //Eh necessário reiniciar o alfabeto
+                while(newIndex<0){
+                    newIndex = 26 + newIndex;
+                }
+                result = result + Dicio[newIndex];
+            }else { //Nao eh necessário reiniciar o alfabeto
+                result = result + Dicio[newIndex];
+            } 
+        } else {  //Nao eh uma letra
+            result = result + atual;
+        }
     }
+    return result;
 }
 
